73a6d2db35f37399911c312a7de2c54c98d82a31
sml <tiqmfk123@naver.com>
2022-01-22 14:11:44 +0900

    Ready for deploy

--- seller/settings.py
+++ seller/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=10000000)
+db_from_env = dj_database_url.config(conn_max_age=500000)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
