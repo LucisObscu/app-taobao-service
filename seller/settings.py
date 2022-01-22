c439dd766f7623544844aa581f32f5bdeeb1adbb
sml <tiqmfk123@naver.com>
2022-01-22 19:22:22 +0900

    Ready for deploy

--- seller/settings.py
+++ seller/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=500000)
+'''
+db_from_env = dj_database_url.config(conn_max_age=10000)
 DATABASES['default'].update(db_from_env)
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
