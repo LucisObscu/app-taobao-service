370da61d45eb06ce8f25e440e73ee4dae2d61046
sml <tiqmfk123@naver.com>
2022-02-03 09:57:09 +0900

    Ready for deploy

--- getdata/views.py
+++ getdata/views.py
@@ -691,7 +691,6 @@ def login(request):
                     if Secret_Key.objects.filter(admin_email=m_email):
                         one = Secret_Key.objects.get(admin_email=m_email)
                         text['key'] = one.key
-                    print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
                     return HttpResponse(json.dumps(text), content_type = "application/json")
             else:
                 print('직원 계정이고')
