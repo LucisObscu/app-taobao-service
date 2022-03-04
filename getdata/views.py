e372238a810e37c53a23860a60d97e62e72ea9bd
sml <tiqmfk123@naver.com>
2022-03-04 13:31:24 +0900

    ok

--- getdata/views.py
+++ getdata/views.py
@@ -1033,8 +1033,7 @@ def get_option_data(request):
             weight = product.weight
             sourcing_pk = one.id
             title = one.org_title
-            option_list = Sourcing_Option_Category.objects.filter(sourcing_id=one)
-            options = json.loads(serializers.serialize("json", option_list))
+            option_list = serializers.serialize("json", Sourcing_Option_Category.objects.filter(sourcing_id=one))
             output.append({'pk':sourcing_pk,'title':title,'weight':weight,'options_list':option_list})
         except:
             pass
