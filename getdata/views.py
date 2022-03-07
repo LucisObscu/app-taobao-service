cd0bc64ff9f7e0edd330a765c21f1438533cc9bf
sml <tiqmfk123@naver.com>
2022-03-07 16:55:36 +0900

    ok

--- getdata/views.py
+++ getdata/views.py
@@ -1050,10 +1050,12 @@ def option_upload(request):
     
     ero_msg = ''
     dt = json.loads(request.body.decode('utf-8'))
+    logger.info(dt)
     for one in Sourcing.objects.filter(id__in = list(dt.keys()),admin_email=admin_email):
         try:
             sourcing_product_one = Sourcing_Product.objects.get(sourcing_id=one)
             sourcing_id = int(str(one.id).strip())
+            logger.info(type(sourcing_id))
             sourcing_one_input = dt[sourcing_id]
             option_id = sourcing_one_input['option_id']
             option_name = sourcing_one_input['option_name']
